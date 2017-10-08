4c6a1320175e361736820b9e24c1ce40992b745f
Scott Schmalz <scott.schmalz@libertymutual.com>
2017-10-08 08:49:25 -0400

    Removes /dist and adds it to .gitignore

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var _csvtojson = require('csvtojson');
-
-var _csvtojson2 = _interopRequireDefault(_csvtojson);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-console.log('hello world!');
